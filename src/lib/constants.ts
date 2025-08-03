a55af7ef27def9aacaf684e380652f1c9a1ba5d9
jeheskielSunloy77 <jeheskielventiokysunloy@gmail.com>
2025-08-03 22:42:00 +0700

    feat: added hero section on index page

---
+++ src/lib/constants.ts
@@ -0,0 +1,23 @@
+import { Github, Linkedin, Mail } from 'lucide-react'
+
+export const BIRTH_DATE = new Date('2000-12-30')
+export const NICK_NAME = 'Jay'
+export const FULL_NAME = 'Jeheskiel Sunloy'
+export const BOT_NAME = 'jBot'
+export const SOCIALS = [
+	{
+		name: 'LinkedIn',
+		href: 'https://www.linkedin.com/in/jeheskiel-ventioky-sunloy',
+		icon: Linkedin,
+	},
+	{
+		name: 'GitHub',
+		href: 'https://github.com/jeheskielSunloy77',
+		icon: Github,
+	},
+	{
+		name: 'Email',
+		href: 'mailto:jeheskielventiokysunloy@gmail.com',
+		icon: Mail,
+	},
+]

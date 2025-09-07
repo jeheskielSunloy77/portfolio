0be24c6d96908902bc3812afd227fe6e958aa57e
jeheskielSunloy77 <jeheskielventiokysunloy@gmail.com>
2025-09-07 17:17:38 +0700

    refactor: remove old toggle function implementation in useTheme hook

--- src/hooks/use-theme.ts
+++ src/hooks/use-theme.ts
@@ -6,21 +6,6 @@ import { theme } from '../stores/theme'
 export function useTheme() {
 	const $theme = useStore(theme)
 
-	// function toggle() {
-	// 	const themeToChange = $theme === 'light' ? 'dark' : 'light'
-	// 	theme.set(themeToChange)
-
-	// 	if (themeToChange === 'dark') {
-	// 		document.documentElement.classList.add('dark')
-	// 		document.documentElement.style.colorScheme = 'dark'
-	// 	} else {
-	// 		document.documentElement.classList.remove('dark')
-	// 		document.documentElement.style.colorScheme = 'light'
-	// 	}
-
-	// 	localStorage.setItem('theme', themeToChange)
-	// }
-
 	const buttonRef = useRef<HTMLButtonElement | null>(null)
 	const toggle = async () => {
 		if (!buttonRef.current) return
